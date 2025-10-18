ef74dc957273ef241c35f64be695560b3a58017c
Andrey Kim <nptm.group@gmail.com>
2025-10-19 00:09:42 +0300

    KAI-42: create homepage sections structure

--- src/components/sections/home/index.ts
+++ src/components/sections/home/index.ts
@@ -1,2 +1,8 @@
 export { default as HeroSection } from "./HeroSection.astro";
+export { default as ServicesSection } from "./ServicesSection.astro";
+export { default as CasesSection } from "./CasesSection.astro";
+export { default as OurPrinciplesSection } from "./OurPrinciplesSection.astro";
+export { default as OurClanSection } from "./OurClanSection.astro";
+export { default as ClientsSection } from "./ClientsSection.astro";
+export { default as FromJournalSection } from "./FromJournalSection.astro";
 export { default as AboutSection } from "./AboutSection.astro";
